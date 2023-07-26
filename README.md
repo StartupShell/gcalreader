5001d7c40736dc42bebb988ec55014c7b9041d79
Wesley Smith <me@wesjd.net>
2023-07-25 21:21:44 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# gcalreader
+
+gcalreader is a Cloudflare Worker that provides a public API for querying Google Calendar events. It is used
+on the startupshell.org website which does not have a backend in order to nicely display upcoming events.
+
+## Getting Started
+
+First and foremost, `npm install`.
+
+### Secrets
+
+You will need to setup the following secrets:
+
+-   `GOOGLE_CALENDAR_ID`
+-   `GOOGLE_CREDENTIALS`
+
+Place the values of these secrets in the file `.dev.vars` in `NAME=VALUE` format, for example:
+
+```
+GOOGLE_CALENDAR_ID=asdklfhasdfkjhasfjkh@group.calendar.google.com
+GOOGLE_CREDENTIALS=askldfasdkjfhgasdfjkhgasdfkhjgasdkfjh....
+```
+
+#### `GOOGLE_CREDENTIALS`
+
+This is the Base64 Hash of the Service Account credentials that will modify the Google Calendar.
+Follow the steps to get this value:
+
+1. Create a Service Account on the [Service Accounts Panel](https://console.cloud.google.com/iam-admin/serviceaccounts?project=multicalconnect)
+2. Click on the Service Account, then go to the "Keys" tab
+3. Click "Add Key" and choose "Create Existing Key". Choose a JSON key type and click "Create". A JSON file will download to your computer.
+4. On your machine, run `cat <downloadedfile> | base64`. Copy this value to the `GOOGLE_CREDENTIALS` secret in `.dev.vars`.
+
+#### `GOOGLE_CALENDAR_ID`
+
+This the ID of the Google Calendar that the API will add events to. You can get the calendar ID by going to
+the [Google Calendar Homepage](https://calendar.google.com/calendar/u/0/r/week), then click the three dots
+on the calendar you want to add events, then click "Settings and sharing". Scroll down to "Integrate Calendar"
+and copy the value under "Calendar ID". It will have the format of `<big hash>@group.calendar.google.com`.
+
+**Important Additional Note**: In order for this Service Account to be able to actually modify the Google Calendar,
+you need to share the calendar with the Service Account. To do so, find your Service Account's email (it's
+in the format `<account name>@<project id>.iam.gserviceaccount.com`). Then, share the Calendar in the same
+"Settings and sharing" page from the above instructions under "Share with specific people or groups". Ensure
+to give the account at least the "Make changes to events" permission level.
+
+### Booting
+
+Simply run `npm run start:local`!
---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import { Hono } from "hono"
+import { Credentials, getAuthToken } from "web-auth-library/google"
+import { array, object, string } from "yup"
+import { DateTime } from "luxon"
+
+type Env = {
+    GOOGLE_CALENDAR_ID: string
+    GOOGLE_CREDENTIALS: string
+}
+type ListItem = {
+    title: string
+    description?: string
+    location?: string
+    start: Date
+    end: Date
+}
+type ListBody = ListItem[]
+
+const gcalDateSchema = object({
+    dateTime: string().required(),
+    timeZone: string().required(),
+})
+const gcalEventItem = object({
+    start: gcalDateSchema.required(),
+    end: gcalDateSchema.required(),
+    summary: string().required(),
+    description: string().optional(),
+    location: string().optional(),
+})
+const gcalListSchema = object({
+    items: array(gcalEventItem).required(),
+})
+
+const parseGoogleCredentials = (raw: string): Credentials => {
+    const decoded = atob(raw) as string
+    return JSON.parse(decoded) as Credentials
+}
+
+const createAuthToken = (credentials: Credentials) =>
+    getAuthToken({
+        credentials,
+        scope: ["https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/calendar.events"],
+    })
+
+export default new Hono<{ Bindings: Env }>()
+    .onError((err, ctx) => {
+        console.error("uncaught error", err)
+        return ctx.json({ error: "internal server error" }, 500)
+    })
+    .get("/list", async (ctx) => {
+        const token = await createAuthToken(parseGoogleCredentials(ctx.env.GOOGLE_CREDENTIALS))
+        const response = await fetch(
+            `https://www.googleapis.com/calendar/v3/calendars/${ctx.env.GOOGLE_CALENDAR_ID}/events?maxResults=10`,
+            {
+                method: "GET",
+                headers: {
+                    authorization: `Bearer ${token.accessToken}`,
+                    "content-type": "application/json",
+                    accept: "application/json",
+                },
+            }
+        )
+        if (response.status != 200) {
+            console.error("failed to fetch events", await response.text())
+            return ctx.json({ status: "fetching events failed" }, 500)
+        }
+
+        const body = await gcalListSchema.validate(await response.json())
+        const result: ListBody = body.items.map((item) => {
+            const startDate = DateTime.fromISO(item.start.dateTime, { zone: item.start.timeZone }).toJSDate()
+            const endDate = DateTime.fromISO(item.end.dateTime, { zone: item.end.timeZone }).toJSDate()
+            return {
+                title: item.summary,
+                start: startDate,
+                end: endDate,
+                description: item.description,
+                location: item.location,
+            }
+        })
+        return ctx.json(result, 200)
+    })
